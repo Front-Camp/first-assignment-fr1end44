f054c8a8a8bedce01e639ded8efe06a253491ad9
fr1end44 <semchuk07@gmail.com>
2018-09-15 15:20:03 +0300

    Update index.js

--- src/sum/index.js
+++ src/sum/index.js
@@ -9,8 +9,22 @@
 * sum(0, 5);   // 5
 * sum(-1, 13); // 12
 */
-const sum = (a, b) => {
-  /* your logic here...*/
-};
+function sum(a,b) {
+    /* your logic here...*/
+    let sum = 0;
+    for(let i=0;i<2;i++){
+      if(arguments[i] !== undefined){
+        if(!isNaN(arguments[i])){
+            if(isFinite(arguments[i])){
+                if(typeof(arguments[i])=== "number" && arguments[i] !== undefined){
+                    sum+=arguments[i];
+                }else{throw new Error};
+            }else{throw new Error};
+        }else{throw new Error};
+      }else{throw new Error};
+    }
+    return sum;
+}
+
 
 export default sum;
